0f6913efe66286439af786e755984db18a207ba7
Jason <jason.j.whittle@gmail.com>
2019-05-06 12:30:47 +1000

    Changed structure to code

--- Wrangling.py
+++ Wrangling.py
@@ -10,7 +10,12 @@ import numpy as np
 print('Libs Imported')
 #endregion
 
-# 2 INPUT VARIABLES----------------------------------------------------------------------------------------
+
+
+# 4 READ MAIN DATA
+#region
+
+# Input Variables
 #region
 # Directory folder of the csv files you want to process
 filename = 'C:\FILES\Hansen-Data-Qualified2.csv'
@@ -18,49 +23,14 @@ filename = 'C:\FILES\Hansen-Data-Qualified2.csv'
 Extension = 'csv'
 # Csv files seperator for input and output files..generally (,) or (|)
 Delimiter = ','
-
-# Column to apply the Swapping to
-ColName = 'COLUMN_NAME'
-
-# Directory file of Lookup Table for swapping
-Input_path_Lookup = 'C:/temp/lookuptable.csv'
-
-# Output folder of the Processed CSV file
-Output_path_processed_csv = 'C:/FILES/'
-
-new_filename = 'DATA-PROCESSED.csv'
-
 print('Directories loaded...')
 #endregion
 
-# 3 READ AND PROCESS THE LOOKUP TABLE----------------------------------------------------------------
-#region
-df_lookup = pd.read_csv(Input_path_Lookup, dtype={'FIND': object, 'REPLACE': object})
-
-# Delete Rows with everything missing in the row
-df_lookup = df_lookup.dropna(axis='index', how='all')
-
-# Delete non Unique (duplicate) FIND rows
-df_lookup.drop_duplicates(subset='FIND', keep=False, inplace=True)
-
-# Create a list of Unique Find items
-List_Subs = df_lookup['FIND'].tolist()
-
-# Change index to FIND
-df_lookup.set_index('FIND', inplace = True)
-
-dict_Subs = df_lookup.to_dict()
-dict_Subs = dict_Subs.get('REPLACE')
-
-print(dict_Subs)
-#endregion
-
-# 4 READ MAIN DATA
-#region
 #sep=Delimiter,"
 df_data = pd.read_csv(filename, engine='python', dtype=str)
 print(df_data.shape)
 print(df_data.head())
+print('Dataframe Loaded...')
 #endregion
 
 # 5 DATA WRANGLING
@@ -74,19 +44,27 @@ print(df_data.head())
 # Checking number of rows
 rows_count = df_data.shape[0]
 # Dropping rows
-df_data = df_data.dropna(axis='index', how='all', inplace=True)
+df_data2 = df_data.dropna(axis='index', how='all')
+df_data = df_data2
 # Checking number of rows removed
 rows_deleted = df_data.shape[0] - rows_count
 # Printing rows removed
 print("Number of Rows Deleted =", rows_deleted)
 #endregion
 
-# 5.1.2 Deleting Rows with missing inflo in certain columns UNFINISHED
+# 5.1.2 Deleting Rows with missing info in certain columns
 #region
+# Input Parameters
+Cols_To_Check = ['WONO', 'SPOTVAL', 'ADDDTTM']
+#note 'any' will delete a row if any of the columns is mssing data
+#note 'all' will only deleter a row if all the columns above are NA.
+How_to_delete = 'any'
 # Checking number of rows
-rows_count = df_data.shape[0] 
+rows_count = df_data.shape[0]
 # Dropping rows
-df_data = df_data.dropna(axis='index', how='all', inplace=True)
+df_data2 = df_data.dropna(axis='index', how = How_to_delete, subset = Cols_To_Check)
+df_data = df_data2
+df_var_checker = df_data.head(5)
 # Checking number of rows removed
 rows_deleted = df_data.shape[0] - rows_count
 # Printing rows removed
@@ -112,7 +90,35 @@ df_data[Name_of_New_Col] = df_data[Cols_To_Join].apply(lambda x: '-'.join(x.map(
 df_data.head()
 #endregion
 
-# 5.4 Swapping Data in a Column
+# 5.4 Swapping Data in a Column based on a csv lookup table
+#region
+
+# Input Params
+#region
+# Column to apply the Swapping to
+ColName = 'COLUMN_NAME'
+# Directory file of Lookup Table for swapping
+# Note: have to columns named 'FIND' and 'REPLACE'
+Input_path_Lookup = 'C:/temp/lookuptable.csv'
+#endregion
+
+# Read and process the lookup table
+#region
+df_lookup = pd.read_csv(Input_path_Lookup, dtype={'FIND': object, 'REPLACE': object})
+# Delete Rows with everything missing in the row
+df_lookup = df_lookup.dropna(axis='index', how='all')
+# Delete non Unique (duplicate) FIND rows
+df_lookup.drop_duplicates(subset='FIND', keep=False, inplace=True)
+# Create a list of Unique Find items
+List_Subs = df_lookup['FIND'].tolist()
+# Change index to FIND
+df_lookup.set_index('FIND', inplace = True)
+dict_Subs = df_lookup.to_dict()
+dict_Subs = dict_Subs.get('REPLACE')
+print(dict_Subs)
+#endregion
+
+# Apply swap changes to dataframe
 #region
 #    Swap the name of the column to rename
 df_data.rename(columns={ColName: 'coltoswapxy'}, inplace=True)
@@ -121,20 +127,20 @@ df_data.coltoswap.replace(dict_Subs , inplace = True)
 #    Swap back the name of the column to rename
 df_data.rename(columns={'coltoswapxy': ColName}, inplace=True)
 print(df_data.head())
+print('DONE SWAPPING')
 #endregion
 
 #5.5 Sorting the Table
 #region
-Columns_Sort_Order = ['SPOTCODE', 'WONO']
+Columns_Sort_Order = ['WONO', 'SPOTCODE']
 df_data.sort_values(by = Columns_Sort_Order, inplace=True)
 print(df_data.head())
 #endregion
 
 #endregion
 
-
-
 # 6 EXPORTING DATA
+#region
 
 # 6.1 Standard Export
 #region
@@ -185,4 +191,6 @@ if PartialChunck == True:
     df_temp.to_csv(path_or_buf=FName, sep=Delimiter, index=False)
 
 print('EXPORTED CHUNKING DONE!!!!!!!!!!!!!!!!!!')
+#endregion
+
 #endregion
