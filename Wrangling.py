e6d1d86f6e5f820c207c607a324700650ce2a959
Jason <jason.j.whittle@gmail.com>
2019-05-06 17:32:08 +1000

    fixed swapping code

--- Wrangling.py
+++ Wrangling.py
@@ -12,13 +12,15 @@ print('Libs Imported')
 
 
 
-# 4 READ MAIN DATA
+
+
+# 2 READ MAIN DATA
 #region
 
 # Input Variables
 #region
 # Directory folder of the csv files you want to process
-filename = 'C:\FILES\Hansen-Data-Qualified2.csv'
+filename = 'C:\FILES\Hansen-Data-Qualified3.csv'
 # Can change to xlsx if needed, other changes will be nessesary to code
 Extension = 'csv'
 # Csv files seperator for input and output files..generally (,) or (|)
@@ -32,6 +34,29 @@ print(df_data.shape)
 print(df_data.head())
 print('Dataframe Loaded...')
 #endregion
+
+
+
+
+
+# 3 VIEWING ISSUES WITH DATA
+#region
+
+# 3.1 Viewing a list of all the unique items in a column
+# Switch WONO with name of the column
+Unique_Array = df_data.WONO.unique()
+Unique_Array.sort()
+#export the Array
+Output_Loc_Filname = r'C:\FILES\UniqueCodes2.csv'
+pd.DataFrame(Unique_Array).to_csv(Output_Loc_Filname)
+
+np.savetxt(Output_Loc_Filname , Unique_Array, delimiter=',', fmt='%s')
+
+#endregion
+
+
+
+
 
 # 5 DATA WRANGLING
 #region
@@ -71,8 +96,29 @@ rows_deleted = df_data.shape[0] - rows_count
 print("Number of Rows Deleted =", rows_deleted)
 #endregion
 
+# 5.1.3 Deleting Rows matching a certain string in a certain column
+#region
+Column_Name_To_Check = "SPOTVAL"
+String_To_Check = ' '
+# Bools_With_String = df_data[Column_Name_To_Check].str.contains(String_To_Check)
+# Bools_With_String = df_data[Column_Name_To_Check].str.startswith(String_To_Check)
+Bools_With_String = df_data[Column_Name_To_Check].str.match(String_To_Check)
+Bools_Without_String = ~Bools_With_String
+df_data2 = df_data[Bools_Without_String]
+Row_Difference = df_data.shape[0] - df_data2.shape[0]
+df_data = df_data2
+print('Rows Deleted: ', Row_Difference)
+
+
+'''Working progress...
+Strings_To_Check = [' ', '  ']
+Columns_To_Check = ['WONO']
+df_data2 = df_data.filter(axis='index', items = Columns_To_Check, regex=' ')
+'''
+
 #endregion
 
+#endregion
 
 # 5.2 Delete Columns
 #region
@@ -92,14 +138,13 @@ df_data.head()
 
 # 5.4 Swapping Data in a Column based on a csv lookup table
 #region
-
 # Input Params
 #region
 # Column to apply the Swapping to
-ColName = 'COLUMN_NAME'
+ColName = 'WONO'
 # Directory file of Lookup Table for swapping
 # Note: have to columns named 'FIND' and 'REPLACE'
-Input_path_Lookup = 'C:/temp/lookuptable.csv'
+Input_path_Lookup = 'C:/FILES/find-replace.csv'
 #endregion
 
 # Read and process the lookup table
@@ -115,6 +160,7 @@ List_Subs = df_lookup['FIND'].tolist()
 df_lookup.set_index('FIND', inplace = True)
 dict_Subs = df_lookup.to_dict()
 dict_Subs = dict_Subs.get('REPLACE')
+print('SUBSTITUTIONS...')
 print(dict_Subs)
 #endregion
 
@@ -123,11 +169,13 @@ print(dict_Subs)
 #    Swap the name of the column to rename
 df_data.rename(columns={ColName: 'coltoswapxy'}, inplace=True)
 #    Make the replacements
-df_data.coltoswap.replace(dict_Subs , inplace = True)
+df_data2 = df_data.coltoswapxy.replace(dict_Subs)
 #    Swap back the name of the column to rename
-df_data.rename(columns={'coltoswapxy': ColName}, inplace=True)
+df_data2.rename(columns={'coltoswapxy': ColName}, inplace=True)
+df_data = df_data2
 print(df_data.head())
 print('DONE SWAPPING')
+#endregion
 #endregion
 
 #5.5 Sorting the Table
@@ -139,13 +187,24 @@ print(df_data.head())
 
 #endregion
 
+
+
+
+
 # 6 EXPORTING DATA
 #region
 
 # 6.1 Standard Export
 #region
-Output_filename = Output_path_processed_csv + new_filename
-df_data.to_csv(path_or_buf=Output_filename, sep= Delimiter, index=False)
+# Input Params
+Output_Location = 'C:/FILES/'
+Output_filename = 'Hansen-Data-Qualified3'
+Output_Extension = '.csv'
+Delimiter = ','
+
+# Creating File
+FName = Output_Location + Output_filename + Output_Extension
+df_data.to_csv(path_or_buf=FName, sep= Delimiter, index=False)
 print('---------------------------------------------')
 print('DONE')
 #endregion
